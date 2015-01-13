92f49138b17be46e1bf173115b99b0e0e2dc5a02
Yuan Ren <y.ren@abdn.ac.uk>
2015-01-13 16:35:07 +0000

    Added with explanations.

---
+++ src/Problems/Largest_Number.py
@@ -0,0 +1,23 @@
+'''
+Created on 13 Jan 2015
+
+@author: Yuan
+'''
+'''
+This problem is essentially a sorting problem:
+Sort all the numbers w.r.t. their first digits, if the same then w.r.t. the second digits, so on and on.
+
+The tricky case is when two numbers X and Y have the same prefix, but one is shorter than the other.
+In this case, X is smaller than Y when X+y is smaller than Y + X.
+'''
+
+class Solution:
+    # @param num, a list of integers
+    # @return a string
+    def largestNumber(self, num):
+        num = [str(x) for x in num]
+        num.sort(cmp=lambda x,y:cmp(y+x, x+y))
+        return "".join(num)
+        
+sol = Solution()
+print sol.largestNumber([1,2,2,2,23,2,3])
